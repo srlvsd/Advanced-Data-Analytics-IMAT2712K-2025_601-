be373e5fcc37a85129e1f0f486221fc187abbafd
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2025-09-25 20:51:50 +0500

    Update lab2_1.py

--- lab2_1.py
+++ lab2_1.py
@@ -15,3 +15,59 @@ print ("Column and rows count:", col_row)
 print("Columns' names:", col_name)
 print("Columns types:", df.dtypes)
 print("Not null count:", non_null)
+
+
+OUTPUT:
+Data info:
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 5960 entries, 0 to 5959
+Data columns (total 13 columns):
+ #   Column   Non-Null Count  Dtype  
+---  ------   --------------  -----  
+ 0   BAD      5960 non-null   int64  
+ 1   LOAN     5960 non-null   int64  
+ 2   MORTDUE  5442 non-null   float64
+ 3   VALUE    5848 non-null   float64
+ 4   REASON   5708 non-null   object 
+ 5   JOB      5681 non-null   object 
+ 6   YOJ      5445 non-null   float64
+ 7   DEROG    5252 non-null   float64
+ 8   DELINQ   5380 non-null   float64
+ 9   CLAGE    5652 non-null   float64
+ 10  NINQ     5450 non-null   float64
+ 11  CLNO     5738 non-null   float64
+ 12  DEBTINC  4693 non-null   float64
+dtypes: float64(9), int64(2), object(2)
+memory usage: 605.4+ KB
+Column and rows count: (5960, 13)
+Columns' names: Index(['BAD', 'LOAN', 'MORTDUE', 'VALUE', 'REASON', 'JOB', 'YOJ', 'DEROG',
+       'DELINQ', 'CLAGE', 'NINQ', 'CLNO', 'DEBTINC'],
+      dtype='object')
+Columns types: BAD          int64
+LOAN         int64
+MORTDUE    float64
+VALUE      float64
+REASON      object
+JOB         object
+YOJ        float64
+DEROG      float64
+DELINQ     float64
+CLAGE      float64
+NINQ       float64
+CLNO       float64
+DEBTINC    float64
+dtype: object
+Not null count: BAD        5960
+LOAN       5960
+MORTDUE    5442
+VALUE      5848
+REASON     5708
+JOB        5681
+YOJ        5445
+DEROG      5252
+DELINQ     5380
+CLAGE      5652
+NINQ       5450
+CLNO       5738
+DEBTINC    4693
+dtype: int64
