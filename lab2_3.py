5afe5433385170e2f1c68c1310594229c5cb4851
Saadat Srailova <105716674+srlvsd@users.noreply.github.com>
2025-09-25 21:43:13 +0500

    Update lab2_3.py

--- lab2_3.py
+++ lab2_3.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import numpy as np
 
+df = pd.read_excel("MobiSortIt_data.xlsx")
+
+category_col = ['category', 'Similar_Problems', 'status', 'issuetype', 'privateproperty', 'assignment']
+df[category_col] = df[category_col].astype('category')
+
+print(df.dtypes)
+
+OUTPUT:
+ticketid               int64
+category            category
+Similar_Problems    category
+duration_days        float64
+status              category
+issuetype           category
+privateproperty     category
+assignment          category
+dtype: object
